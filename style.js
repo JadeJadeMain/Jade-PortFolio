d91e62ee4797a92cef71ea2eff48d9a9ce3afb9a
JadeJadeMain <jadecotonganformentera@gmail.com>
2025-02-26 21:01:51 -0200

    new update js css and html

--- style.js
+++ style.js
@@ -1,3 +1,5 @@
+
+
 let texts = "Frontend Developer";
 let animatedtexts = document.querySelector('.nameJade');
 let indexChar = 0
@@ -39,3 +41,63 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("popup").style.display = "none";
     });
 });
+
+
+
+// for Email users send
+
+(function(){
+    emailjs.init("kbvhfzKBrzaar5NsR"); // Your Public Key
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let Username = document.getElementById('name');
+    let Useremail = document.getElementById('email');
+    let UsreMessage = document.getElementById('message');
+    let buttonSend = document.getElementById('btnSend');
+
+
+    let serviceID = "service_6bamp6j";  // Your Service ID
+    let templateID = "template_xwlo02t"; // Your Template ID
+
+    let templateParams = {
+        from_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send(serviceID, templateID, templateParams)
+        .then(function(response) {
+            Swal.fire({
+                title: "Success!",
+                width: "200px",
+                text: "Message sent successfully!",
+                icon: "success",
+                confirmButtonText: "OK"
+            });
+
+            Username.value = "";
+            Useremail.value = "";
+            UsreMessage.value= "";
+
+            console.log("Success:", response);
+        })
+        .catch(function(error) {
+            Swal.fire({
+                icon: "error",
+                width: "200px",
+                title: "Oops...",
+                text: "Something went wrong!",
+                footer: '<a href="#">Why do I have this issue?</a>'
+              });
+
+            Username.value = "";
+            Useremail.value = "";
+            UsreMessage.value= "";
+            console.error("Error:", error);
+        });
+});
+
+
